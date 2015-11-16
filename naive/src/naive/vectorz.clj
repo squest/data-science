31798e6ff125d9f382a9526b7c895bb3b437c630
squest <sabdaps@gmail.com>
2015-11-16 17:27:07 +0700

    vector operations

--- naive/src/naive/vectorz.clj
+++ naive/src/naive/vectorz.clj
@@ -5,3 +5,18 @@
   [v1 v2]
   (reduce + (map + v1 v2)))
 
+(defn add
+  "Returns the addition of all n-dimensional vectors"
+  [& vs]
+  (vec (reduce #(map + % %2) vs)))
+
+(defn abs
+  "Returns the scalar abs of a vector"
+  [v]
+  (Math/sqrt (map #(* % %) v)))
+
+(defn points->vec
+  "Returns the vector value from two n-dimensional points"
+  [p1 p2]
+  (mapv - p2 p1))
+
