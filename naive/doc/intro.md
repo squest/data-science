5355da4c4a73613147aff2867fcf5da8e35e2743
squest <sabdaps@gmail.com>
2015-11-16 07:07:54 +0700

    naive implementations

---
+++ naive/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to naive
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ naive/test/naive/core_test.clj
@@ -0,0 +1,7 @@
+(ns naive.core-test
+  (:require [clojure.test :refer :all]
+            [naive.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
