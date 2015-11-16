c7f8ef86dca9e2d589ee1dc2fb8ed5a9759a5545
squest <sabdaps@gmail.com>
2015-11-16 22:14:58 +0700

    more vector operations

--- naive/src/naive/vectorz.clj
+++ naive/src/naive/vectorz.clj
@@ -13,10 +13,24 @@
 (defn abs
   "Returns the scalar abs of a vector"
   [v]
-  (Math/sqrt (map #(* % %) v)))
+  (Math/sqrt (reduce + (map #(* % %) v))))
 
 (defn points->vec
   "Returns the vector value from two n-dimensional points"
   [p1 p2]
   (mapv - p2 p1))
 
+(defn scalar-times
+  "Returns a scalar multiplication of vector v with constant k"
+  [k v]
+  (mapv * (repeat k) v))
+
+(defn project
+  "Return the orthogonal projection of vector a in b. When given three
+  arguments, it will return the vector version"
+  ([a b]
+     (/ (dot a b) (abs b)))
+  ([a b k]
+     (let [abs-b (abs b)
+           abs-x (/ (dot a b) abs-b)]
+       (scalar-times (/ abs-x abs-b) b))))
