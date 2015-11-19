7aaaf27f5770156cfa6dd459d08d004df36d4be8
davin <dizqar@gmail.com>
2015-11-19 21:19:41 +0700

    @squest
    ini percobaan yang sangat amatir. tolong dimaklumi.

--- training/teraining/src/teraining/core.clj
+++ training/teraining/src/teraining/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn means
+  ([coll] (float (/ (reduce + coll) (count coll))))
+  ([keys maps] (let [select (map #(select-keys % keys) maps)
+                     fre (int (count select))]
+                 (->> (zipmap keys (repeat fre))
+                      (merge-with / (reduce #(merge-with + % %2) select))
+                      (vals)
+                      (map int)
+                      (zipmap keys)))))
--- training/teraining/test/teraining/core_test.clj
+++ training/teraining/test/teraining/core_test.clj
@@ -1,8 +1,21 @@
 (ns teraining.core-test
   (:require [clojure.test :refer :all]
-            [teraining.core :refer :all
-             ]))
+            [teraining.core :refer :all]))
 
 (deftest a-test
   (testing "FUCK MEH!!!!!!!!!!!!!!!!!!!!!!!!"
-    (is (nil? 0 1))))
+    (is (= 1 (/ 30 (* 5 3 (- 10 4 (/ 24 6))))))))
+
+
+(deftest statistics
+  (testing "this is what i mean"
+
+    (is #(= (int (/ (reduce + %) (count %)))
+            (means %))[1 2 3 3 1 2 443 12])
+
+    (is (= {:angka 426, :beda 226, :cara 53}
+           (#(means % %2) [:angka :beda :cara]
+             [{:tampan 0 :angka 300 :pepet 30 :beda 50 :cara 150}
+              {:tampan 0 :angka 909 :pepet 30 :beda 40 :cara 10}
+              {:tampan 0 :angka 69 :pepet 303 :beda 588 :cara 1}]))))
+  (testing "this is what i frequencies"))
