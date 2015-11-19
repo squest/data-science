d96e3916df7f1f415ac0cf60b6b80344bceee835
davin <dizqar@gmail.com>
2015-11-19 18:23:11 +0700

    @squest

---
+++ training/teraining/test/teraining/core_test.clj
@@ -0,0 +1,8 @@
+(ns teraining.core-test
+  (:require [clojure.test :refer :all]
+            [teraining.core :refer :all
+             ]))
+
+(deftest a-test
+  (testing "FUCK MEH!!!!!!!!!!!!!!!!!!!!!!!!"
+    (is (nil? 0 1))))
