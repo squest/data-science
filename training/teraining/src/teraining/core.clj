8b4b3dc0f66815c93ffddc88cd08c546ff953531
davin <dizqar@gmail.com>
2015-11-20 03:52:04 +0700

    @squest
    marsupilia

--- training/teraining/src/teraining/core.clj
+++ training/teraining/src/teraining/core.clj
@@ -5,6 +5,33 @@
   [x]
   (println x "Hello, World!"))
 
+
+
+(defn quartiles
+  [coll]
+  (let [median (fn median
+          [coll]
+          (if (even? (count coll))
+            (->> (/ (+ (nth coll (/ (count coll) 2)) (dec (nth coll (/ (count coll) 2)))) 2)
+                 float)
+            (nth coll (/ (count coll) 2))))
+
+        quartile1(fn quartile1
+          [coll]
+          (->> (/ (+ (nth coll (int (float (+ (/ (count coll) 4) (/ 1 4)))))
+                     (nth coll (dec (float (+ (/ (count coll) 4) (/ 1 4))))))
+                  2)
+               float))
+
+        quartil3(fn quartile3
+          [coll]
+          (float (/ (+ (nth coll (int (float (+ (* 3 (/ (count coll) 4)) (/ 3 4)))))
+                       (nth coll (dec (int (float (+ (* 3 (/ (count coll) 4)) (/ 3 4)))))))
+                    2)))]
+    [(quartile1 coll) (median coll) (quartil3 coll)]))
+
+
+
 (defn means
   ([coll] (int (/ (reduce + coll) (count coll))))
   ([keys maps] (let [select (map #(select-keys % keys) maps)
@@ -21,11 +48,30 @@
 
 (defn variance
   ([collys] (cond
-     (= (count collys) 0) "Jangan goblok"
-     (= (count collys) 1) "ngentot lo"
+     (= (count collys) 0) "ばか"
+     (= (count collys) 1) "おいしい"
      :else (->> (map #(kalipangkat % 2) collys)
                 (reduce +)
                 (#(/ % (count collys)))
                 float)))
   ([yg-pengen-diambil coll] (->> (for [k yg-pengen-diambil] (variance (map #(k %) coll)))
                                  (zipmap yg-pengen-diambil))))
+
+(defn standard-deviation
+  ([x] (if (or (= (count x) 0)
+           (= (count x) 1))
+     "坂本"
+     (->> (map #(- % (means x)) x)
+          (map #(kalipangkat % 2))
+          (reduce +)
+          (#(/ % (count x)))
+          Math/sqrt)))
+  ([yg-pengen-diambil coll] (->> (for [s yg-pengen-diambil] (standard-deviation (map #(s %) coll)))
+                                 (zipmap yg-pengen-diambil))))
+
+
+(defn mode
+  ([coll] (apply max-key val (frequencies coll)))
+  ([yg-pengen-diliat sicol] (zipmap yg-pengen-diliat (for [p yg-pengen-diliat] (mode (map #(p %) sicol))))))
+
+
--- training/teraining/test/teraining/core_test.clj
+++ training/teraining/test/teraining/core_test.clj
@@ -2,10 +2,16 @@
   (:require [clojure.test :refer :all]
             [teraining.core :refer :all]))
 
+
 (deftest a-test
   (testing "FUCK MEH!!!!!!!!!!!!!!!!!!!!!!!!"
     (is (= 1 (/ 30 (* 5 3 (- 10 4 (/ 24 6))))))))
 
+(def coll [{:patung 150 :pancoran 150 :dalam 91 :pejam  150 :bulan 123}
+           {:patung 50 :pancoran 15 :dalam 9 :pejam  15 :bulan 12}
+           {:patung 1500 :pancoran 1520 :dalam 19 :pejam  50 :bulan 10}])
+
+(def yg-pgn-diambil [:pancoran :dalam :bulan] )
 
 (deftest statistics
   (testing "if you know what i mean"
@@ -23,14 +29,28 @@
               {:tampan 0 :angka 909 :pepet 30 :beda 40 :cara 10}
               {:tampan 0 :angka 69 :pepet 303 :beda 588 :cara 1}]))))
 
-  (testing "if you know what i variance"
+  (testing "he is so variance"
     (is (= 2.5
            (variance [1 2])))
-    (is (= "Jangan goblok"
+    (is (= "ばか"
            (variance [])))
     (is (= 3283.5
-           (variance (range 100))))
-    (is (= {:pancoran 777708.3, :dalam 2907.6667, :bulan 5124.3335}
-           (#(variance % %2) [:pancoran :dalam :bulan][{:patung 150 :pancoran 150 :dalam 91 :pejam  150 :bulan 123}
-                                                       {:patung 50 :pancoran 15 :dalam 9 :pejam  15 :bulan 12}
-                                                       {:patung 1500 :pancoran 1520 :dalam 19 :pejam  50 :bulan 10}] )))))
+           (variance (range 100)))))
+
+  (testing "a standard man knows that deviation knows nothing"
+    (is (= 288.38631497813253
+           (standard-deviation (range 1 1000))))
+    (is (= {:pancoran 679.8818524812478, :dalam 36.53309002352069, :bulan 52.80467151051442}
+           (standard-deviation yg-pgn-diambil coll))))
+
+  (testing "Bahe-mode"
+    (is (= [1 7]
+           (mode [1 1 1 2 2 3 3 2 2 1 1 1 1])))
+    (is (= {:pancoran [1520 1], :dalam [19 1], :bulan [10 1]}
+           (mode yg-pgn-diambil
+                  coll))))
+
+  (testing "quartilians"
+    (is (= [21.0 23 26.5]
+           (quartiles [19 21 21 22 23 24 25 28 30])))))
+
