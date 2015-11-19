4aa4a34164250867793b2d427b0245ef0493ac04
davin <dizqar@gmail.com>
2015-11-20 04:01:11 +0700

    Summary while i burst into flames

--- training/teraining/test/teraining/core_test.clj
+++ training/teraining/test/teraining/core_test.clj
@@ -24,10 +24,9 @@
            (means (range 11))))
 
     (is (= {:angka 426, :beda 226, :cara 53}
-           (#(means % %2) [:angka :beda :cara]
-             [{:tampan 0 :angka 300 :pepet 30 :beda 50 :cara 150}
-              {:tampan 0 :angka 909 :pepet 30 :beda 40 :cara 10}
-              {:tampan 0 :angka 69 :pepet 303 :beda 588 :cara 1}]))))
+            (means [:angka :beda :cara] [{:tampan 0 :angka 300 :pepet 30 :beda 50 :cara 150}
+                                         {:tampan 0 :angka 909 :pepet 30 :beda 40 :cara 10}
+                                         {:tampan 0 :angka 69 :pepet 303 :beda 588 :cara 1}]))))
 
   (testing "he is so variance"
     (is (= 2.5
